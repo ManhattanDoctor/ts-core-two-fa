24ca8e662f11b2b8cbc1e9bf1ff8bdb083909511
Renat Gubaev <>
2021-03-23 16:29:20 +0300

    auto commit

--- src/dto/ITwoFaResetStartDto.ts
+++ src/dto/ITwoFaResetStartDto.ts
@@ -1,9 +1,9 @@
 import { ITraceable } from '@ts-core/common/trace';
-import { TwoFaOwnerUid } from '../ITwoFa';
+import { ITwoFa, TwoFaOwnerUid } from '../ITwoFa';
 
 export interface ITwoFaResetStartDto extends ITraceable {
     ownerUid: TwoFaOwnerUid;
     type: string;
 }
 
-export type ITwoFaResetStartDtoResponse = string;
+export type ITwoFaResetStartDtoResponse = ITwoFa;
